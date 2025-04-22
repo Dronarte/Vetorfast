68bedbcdcff072f01bc7f4479cb379ec0e82ed2b
Dronarte <dronartegraficarapida@gmail.com>
2025-04-21 21:04:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_cors import CORS
 import os
 import io
 from PIL import Image
-
 import numpy as np
+import potrace
 
 app = Flask(__name__)
 CORS(app)
@@ -19,37 +19,35 @@ def vectorize():
         return jsonify({"error": "Nenhum arquivo enviado"}), 400
 
     image_file = request.files['image']
-    img = Image.open(image_file).convert("L")  # Converte para escala de cinza
-    bitmap = np.array(img) < 128  # Binariza
+    image = Image.open(image_file).convert("L")  # escala de cinza
+    image = image.point(lambda x: 0 if x < 128 else 1, '1')  # binariza
 
-    bmp = potrace.Bitmap(bitmap)
-    path = bmp.trace()
+    bmp = np.array(image, dtype=np.uint32)
+    bmp = np.where(bmp == 0, 1, 0)  # inverte
 
-    svg_data = io.StringIO()
-    svg_data.write('<?xml version="1.0" standalone="no"?>\n')
-    svg_data.write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n')
+    bitmap = potrace.Bitmap(bmp)
+    path = bitmap.trace()
 
+    svg_io = io.StringIO()
+    svg_io.write('<svg xmlns="http://www.w3.org/2000/svg">\n')
     for curve in path:
-        svg_data.write('<path d="')
+        svg_io.write('<path d="')
+        start = curve.start_point
+        svg_io.write(f'M {start[0]} {start[1]} ')
         for segment in curve:
-            start = segment.start_point
             if segment.is_corner:
                 c = segment.c
-                svg_data.write(f'M {start.x},{start.y} L {c.x},{c.y} ')
+                end = segment.end_point
+                svg_io.write(f'L {c[0]} {c[1]} {end[0]} {end[1]} ')
             else:
-                c1, c2, end = segment.c1, segment.c2, segment.end_point
-                svg_data.write(f'M {start.x},{start.y} C {c1.x},{c1.y} {c2.x},{c2.y} {end.x},{end.y} ')
-        svg_data.write('" stroke="black" fill="none"/>\n')
+                c1, c2 = segment.c1, segment.c2
+                end = segment.end_point
+                svg_io.write(f'C {c1[0]} {c1[1]} {c2[0]} {c2[1]} {end[0]} {end[1]} ')
+        svg_io.write('Z" fill="black"/>\n')
+    svg_io.write('</svg>')
 
-    svg_data.write('</svg>')
-    svg_data.seek(0)
-
-    return send_file(
-        io.BytesIO(svg_data.getvalue().encode()),
-        mimetype='image/svg+xml',
-        as_attachment=True,
-        download_name="imagem_vetorizada.svg"
-    )
+    svg_bytes = io.BytesIO(svg_io.getvalue().encode("utf-8"))
+    return send_file(svg_bytes, mimetype='image/svg+xml', as_attachment=True, download_name="vetorizado.svg")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
