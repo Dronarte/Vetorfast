92b4d244b1f9f570bfb667ce4acd7a9df838fed7
Dronarte <dronartegraficarapida@gmail.com>
2025-04-21 21:47:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS
 import os
 import io
 import numpy as np
 from PIL import Image
-from skimage import measure
-import svgwrite
+from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
+import potrace
 
 app = Flask(__name__)
 CORS(app)
@@ -21,19 +20,34 @@ def vectorize():
 
     image_file = request.files['image']
     image = Image.open(image_file).convert("L")  # escala de cinza
-    image = np.array(image)
-    image = np.where(image < 128, 1, 0)  # binarização (1 = preto)
+    image = image.point(lambda x: 0 if x < 128 else 1, '1')  # binariza
 
-    contours = measure.find_contours(image, 0.8)
-    
-    dwg = svgwrite.Drawing(size=image.shape[::-1])
-    for contour in contours:
-        points = [(int(p[1]), int(p[0])) for p in contour]
-        if len(points) > 2:
-            dwg.add(dwg.polygon(points=points, fill='black'))
+    bmp = np.array(image, dtype=np.uint32)
+    bmp = np.where(bmp == 0, 1, 0)  # inverte: 1 = preto, 0 = branco
 
-    svg_io = io.BytesIO()
-    dwg.write(svg_io)
-    svg_io.seek(0)
+    bitmap = potrace.Bitmap(bmp)
+    path = bitmap.trace()
 
-    return send_file(svg_io, mimetype='image/svg+xml', as_attachment=True, download_name='vetorfast.svg')
+    svg = io.StringIO()
+    for curve in path:
+        svg.write('<path d="M ')
+        for segment in curve:
+            if segment.is_corner:
+                c = segment.c
+                svg.write(f'L {c[0][0]} {c[0][1]} {c[1][0]} {c[1][1]} ')
+            else:
+                c = segment.c
+                svg.write(f'C {c[0][0]} {c[0][1]}, {c[1][0]} {c[1][1]}, {c[2][0]} {c[2][1]} ')
+        svg.write('Z" fill="black"/>\n')
+    svg.write('</svg>')
+
+    return send_file(
+        io.BytesIO(svg.getvalue().encode()),
+        mimetype='image/svg+xml',
+        download_name='vetorizado.svg',
+        as_attachment=True
+    )
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
